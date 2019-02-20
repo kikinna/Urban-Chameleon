329871e68494dfe49850f9dc097b828eda63fe32
LordNene <batrna.jozef@gmail.com>
2019-02-20 11:46:40 +0100

    map component which might actually work this time

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,63 @@
 <template>
   <div id="app">
-    <TheMap />
+    <Mapbox
+        access-token="pk.eyJ1Ijoic2hvbmdvbG9sbyIsImEiOiJjamVoN25yYTQxMXBwMzNuc2ZkeGk5eGtzIn0.ZQNxwHhtZDBfsVNjDL0c7A"
+        :map-options="{
+            style: 'https://maps.tilehosting.com/styles/positron/style.json?key=erAyQhECgFpHi6K8tzqm',//'mapbox://styles/mapbox/light-v9',
+            center: [16.606837, 49.195060],
+            zoom: 10
+        }"
+        :geolocate-control="{
+					show: true, 
+					position: 'top-left',
+					options: {
+						positionOptions: {
+							enableHighAccuracy: true
+						},
+						trackUserLocation: true
+					}
+				}"
+				:scale-control="{
+					show: true,
+					position: 'top-left'
+				}"
+				:fullscreen-control="{
+					show: true,
+					position: 'top-left'
+				}"
+				@map-init="mapInit"
+        @map-click="mapClicked"
+				@map-mousemove="mapMouseMoved"
+				@geolocate-geolocate="geolocate"
+    >
+    </Mapbox>
   </div>
 </template>
 
 <script>
-import TheMap from './components/TheMap.vue'
+import Mapbox from './components/Mapbox.vue'
 
 export default {
   name: 'app',
   components: {
-     TheMap
+     Mapbox
   },
   methods: {
-    doStuff() {
-      
-    }
+    mapInit(map) {
+			const Draw = new MapboxDraw();
+
+      map.addControl(Draw);
+    },
+    mapClicked(map, e) {
+			console.log(map.getZoom());
+		},
+		mapMouseMoved(map, e) {
+			const features = map.queryRenderedFeatures(e.point, { layers: ['points'] });
+			map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
+		},
+		geolocate(control, position) {
+			console.log(`User position: ${position.coords.latitude}, ${position.coords.longitude}`);
+		}
   }
 }
 
@@ -31,4 +73,10 @@ export default {
   margin-top: 60px;
 }
 
+ #map {position: absolute; top: 0; right: 0; bottom: 0; left: 0;}
+
 </style>
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-import Mapbox from 'mapbox-gl-vue';
+//import Mapbox from 'mapbox-gl-vue';
 
 Vue.config.productionTip = false
 
@@ -9,7 +9,10 @@ new Vue({
   //el: '#app',
   store,
   components: {
-      Mapbox
+
   },
+  
   render: h => h(App),
 }).$mount('#app')
+
+
