c4cd4ecc2de9b7881e1375232bec66b23afc234a
LordNene <batrna.jozef@gmail.com>
2019-02-22 12:41:50 +0100

    :(

--- src/store.js
+++ src/store.js
@@ -5,12 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    map: {},
+    container: {}
   },
   mutations: {
-
+    loadMap (state, map) {
+      //console.log("nonmutation", container);
+      state.map = map;
+      state.container = map.getCanvasContainer();
+      console.log("mutation canvas", state.container);
+      //state.mapLoaded = true
+    }
   },
   actions: {
-
+    loadMap (context) {
+      context.commit('loadMap', map, container);
+    }
   }
 })
