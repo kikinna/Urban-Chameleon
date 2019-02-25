863ed2d3a95f66d5fdf09ed4c6480cd8c0452d07
kikinnaz <zakopcanova.k@gmail.com>
2019-02-25 11:31:41 +0100

    Added absolute position to svg element

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,10 @@ module.exports = {
     'eslint:recommended'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/no-unused-vars': 'warning',
+    'vue/valid-v-html': 'warning'
   },
   parserOptions: {
     parser: 'babel-eslint'
