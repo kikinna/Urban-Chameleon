9b79c69c295c43e824c48362f92668d15b333153
Jozef Bátrna <batrna.jozef@gmail.com>
2019-02-26 10:34:10 +0100

    kiki skusa git rebase

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 //import Mapbox from 'mapbox-gl-vue';
+import * as d3 from "d3";
 
 Vue.config.productionTip = false
 
-import * as d3 from "d3";
+
 
 new Vue({
   //el: '#app',
