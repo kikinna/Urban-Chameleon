28e4164b0eee6d33828153b349a32bc033c98af9
Magdalena Kejstova <xkejsto1@fi.muni.cz>
2019-03-07 10:58:34 +0100

    Edited detailWindow - missing if and some small details

--- src/components/AccidentDetail.vue
+++ src/components/AccidentDetail.vue
@@ -1,19 +1,32 @@
 <template>
-    <div class = 'accidentDetail' >
-        <p class='title_style'> {{parentdata[0].Datum}} <br> {{parentdata[0].Den}} </p>
-        <p class='text_style'> <em> Druh nehody: &ensp; </em>{{parentdata[0].DruhNehody}} <br> 
-                             <em> Hlavní příčina: &ensp; </em>{{parentdata[0].HlavniPricina}} <br> 
+    
+    <div class = 'accidentDetail' id="svgContainer" >
+        <!--<circle cx=280 cy=70 r=60 style="fill:#5490b0cd"></circle>-->
+        <svg width="390" height="73">
+            <text class="text_style2" x="270" y="30" >{{parentdata[0].Datum }} </text>
+            <text class="text_style2" x="270" y="50" >{{parentdata[0].Den }} </text>
+            <image  v-bind="{'xlink:href':'adress[parentdata[0].DenNoc'}"  x="340" y="20" width="25" height="25"></image>
+            <rect x="100" y="70" width="275" height="3" fill="#60bac6"/>
+        </svg>
+        <p class='title_style'> {{parentdata[0].DruhNehody}}  </p>
+        <p class='text_style'> <em> Hlavní příčina: &ensp; </em>{{parentdata[0].HlavniPricina}} <br> 
                              <em> Zavineni: &ensp; </em>{{parentdata[0].Zavineni}}<br> 
                              <em> Stav povrchu: &ensp; </em>{{parentdata[0].StavPovrchu}} <br> 
                              <em> Počasí: &ensp; </em>{{parentdata[0].Pocasi}}<br> 
                              <em> Alkohol: &ensp; </em>{{parentdata[0].Alkohol}}</p>
-       <svg style="width:390; height:160">
-            <!--<circle :cx='cx' :cy='cy' r=50></circle>-->
-            <circle cx=280 cy=70 r=60 style="fill:#5490b0cd"></circle>
-        </svg> 
+        <svg width="390" height="73">
+            <text class="text_style2" x="15" y="30" >{{"Usmrceno: "}} </text>
+            <text class="text_style2" x="100" y="30" >{{parentdata[0].Usmrceno + " x"}} </text>
+            <image  xlink:href="../assets/body.png"  x="117" y="12" width="22" height="22"></image>
 
+            <text class="text_style2" x="15" y="53" >{{"Těžce zranení: "}} </text>
+            <text class="text_style2" x="122" y="53" >{{parentdata[0].Tezce + " x"}} </text>
+            <image  xlink:href="../assets/body.png"  x="139" y="36" width="22" height="22"></image>
 
-
+            <text class="text_style2" x="180" y="53" >{{"Lehce zranení: "}} </text>
+            <text class="text_style2" x="290" y="53" >{{parentdata[0].Lehce + " x"}} </text>
+            <image  xlink:href="../assets/body.png"  x="307" y="36" width="22" height="22"></image>
+        </svg>
     </div>
 </template>
 
@@ -27,7 +40,16 @@
         props:{
             title: String,
             parentdata: Object,
+            
         }, 
+        data(){
+            return{
+                adress:{
+                    Noc : "../assets/Noc.png",
+                    Den : "../assets/Den.png",
+                }
+            }
+        },
         mounted(){
             //this.loadData();
             this.init();
@@ -41,17 +63,7 @@
                 this.render();
             },
             render(){
-
-                let svg = d3
-                    //.select(this.$store.state.map.getCanvasContainer) //chybalo state
-                    .select(this.$el) //ale zrovna v tomto pripade chces pouzit "tento element" co je ten div z template... zatial to ber ako magiu, mozem vysvetlit nazivo :D ale je to kvoli tomu, ze AccidentDetail je vnutri Visualization
-                    .append("svg") //this bit is important
-                    .attr("class","accidentDetail") //asi je jedno ci je to id alebo class, ale potom sa to lisi v css, class ma ".accidentDetail", id myslim "#acc..."
-                    .attr("width", 250)
-                    .attr("height", 350)
-                    .attr("min-x",100)
-                    .attr("min-y",100)
-
+                
             },
         }
     }
@@ -69,21 +81,41 @@
     
 
 }
-.title_style{
+.date_style{
     color:rgb(0, 0, 0);
-    font-family: Helvetica;
-    font-size: 20px;
+    font-family: 'Avenir';
+    font-size: 15px;
     text-align: end;
     font-weight: 300;
     transform: translate(-10%, 0%)
 }
+.title_style{
+    color:rgb(0, 0, 0);
+    font-family: 'Avenir';
+    font-size: 18px;
+    font-weight: 300;
+    transform: translate(6%, 0%)
+}
 .text_style{
     color : black;
-    font-family: Helvetica;
+    font-family: 'Avenir';
     font-size: 14px;
     transform: translate(+4%, 0%);
     font-weight: 300;
     line-height: 1.5;
+    width: 380px;
+    height: 105px;
+    padding-bottom: 0;
+    border-bottom: 0;
     
+}
+.text_style2{
+    color:rgb(0, 0, 0);
+    font-family: 'Avenir';
+    font-size: 15px;
+    font-weight: 300;
+}
+.line{
+    fill:#60bac6
 }
 </style>
