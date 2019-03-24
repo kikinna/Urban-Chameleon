20fba25bf7b07a8a61ca452c942f0a55f072db7f
kikinnaz <zakopcanova.k@gmail.com>
2019-03-24 17:00:17 +0100

    Added devicePixelRatio and attempt to read colors from pixels.

--- src/components/GaussPreprocess.vue
+++ src/components/GaussPreprocess.vue
@@ -21,7 +21,9 @@ import {
             return {
                 paper: null,
                 accident_dots: [],
-                canvas: null
+                canvas: null,
+                devicePixelRatio: 1,
+                canvas_context: null
             }
         }, 
         created() {
@@ -32,25 +34,37 @@ import {
         mounted() {
             const viewport = getViewport(this.$store.state.map);
 
-            this.canvas = document.getElementById('paper-canvas')
+            this.canvas = document.getElementById('paper-canvas');
             this.canvas.width = window.innerWidth;
             this.canvas.height = window.innerHeight;
+            this.devicePixelRatio = window.devicePixelRatio || 1;
+            this.canvas_context = this.canvas.getContext("2d")
+            console.log(this.canvas_context)
 
+            let temp_pos;
             this.paper = Paper.setup(this.canvas);
             
-            // console.log(this.paper)
-            console.log(accidentData)
             accidentData.accidents.map(d => {
                 let pos = viewport.project([d.X, d.Y])
-                // console.log(pos)
-                this.accident_dots.push(new Paper.Path.Circle(new Paper.Point(pos[0], pos[1]), 8).fillColor = 'pink')
+                temp_pos = pos;
+                
+                this.accident_dots.push(new Paper.Path.Circle(new Paper.Point(pos[0] / this.devicePixelRatio, pos[1] / this.devicePixelRatio), 8 / this.devicePixelRatio).fillColor = 'pink')
                 // this.accident_dots.push(new Paper.Path.Circle(new Paper.Point(pos.x, pos.y), 8).fillColor = 'pink')
 
             })
             
-            var myCircle = new Paper.Path.Circle(new Paper.Point(100, 70), 50);
-            myCircle.fillColor = 'black';
+            // var myCircle = new Paper.Path.Circle(new Paper.Point(100, 70), 50);
+            // myCircle.fillColor = 'black';
             Paper.view.draw();
+
+            
+             // set color now
+            // var canvasColor = this.context.getImageData(Math.random()*this.canvas.width, Math.random()*this.canvas.width, 1, 1).data; // rgba e [0,255]
+            var canvasColor = this.canvas_context.getImageData(temp_pos[0] / this.devicePixelRatio, temp_pos[0] / this.devicePixelRatio, 1, 1).data; // rgba e [0,255]
+            var r = canvasColor[0];
+            var g = canvasColor[1];
+            var b = canvasColor[2];
+            console.log(r, g, b)
 
         }
 
